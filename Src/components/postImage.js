2e3d4106a5e06cbc4e6e2cb69a6ed7f0434a1ba1
Miguel Scarpetta <josemiguelscarpetta@gmail.com>
2026-04-07 18:21:31 -0500

    Feat: Improved post.js and postImage.js, added emotions, added local storage

--- Src/components/postImage.js
+++ Src/components/postImage.js
@@ -1,51 +1,22 @@
-export class PostImage {
-    constructor(avatar, nombreUsuario, content, image, emocion, likes, comments) {
-        this.avatar = avatar;
-        this.nombreUsuario = nombreUsuario;
-        this.content = content;
-        this.image = image;
-        this.emocion = emocion;        
-        this.likes = likes;
-        this.comments = comments;
-    }
-    render() {
-        const html = `
-        <style>
-            .username {
-                font-weight: bold;
-                font-size: 18px;
-            }
-            .avatar {
-                width: 50px;
-                height: 50px;
-                border-radius: 50%;
-                margin-right: 10px;
-            }
-            .emotion {
-                width: 30px;
-                height: 30px;
-                margin-left: 10px;
-                }
-                .content {
-                    font-weight: bold;
-                    font-size: 16px;
-                    }
-                </style>
-        <div>
-        <div>
-            <h3 class="username">${this.nombreUsuario}</h3>
-            <img src="${this.avatar}" alt="Avatar" class="avatar">
-            <img src="${this.emocion}" alt="Emotion" class="emotion">
-        </div>
-        <p class="content">${this.content}</p>
-        <img src="${this.image}" alt="Post Image" class="post-image">
-        <p class="likes">❤️${this.likes}</p>
-        <p class="comments">💬${this.comments}</p>
-    </div>` 
-        
-    const postContainer = document.getElementById('postContainer');
-        postContainer.innerHTML += html;
-    }
+import Post from './post.js';
+
+export class PostImage extends Post {
+  /**
+   * Extiende Post agregando soporte para imagen.
+   * @param {Object} data - Igual que Post, pero con data.image (URL de la imagen)
+   */
+  constructor(data) {
+    super(data);
+    this.image = data.image;
+  }
+
+  // Solo sobreescribe el body para incluir la imagen
+  renderBody() {
+    return `
+      <p class="post__content">${this.content}</p>
+      <img src="${this.image}" alt="Post image" class="post__image">
+    `;
+  }
 }
 
 export default PostImage;
--- index.js
+++ index.js
@@ -1,27 +1,15 @@
-import { faker } from './node_modules/@faker-js/faker/dist/index.js';
-console.log(faker.person.fullName());
 import Post from './Src/components/post.js';
 import PostImage from './Src/components/postImage.js';
 
-const post1 = new Post(
-    faker.image.avatar(),
-    faker.person.fullName(),
-    "content",
-    "./Src/assets/Triste.png",
-    10,
-    5,
-)
+async function init() {
+  const response = await fetch('./Src/postdat.json');
+  const posts = await response.json();
 
-const post2 = new PostImage(
-    faker.image.avatar(),
-    faker.person.fullName(),
-    "content",
-    faker.image.url(),
-    "./Src/assets/Triste.png",
-    10,
-    5
-);
+  posts.forEach(data => {
+    // Si el post tiene imagen → PostImage, si no → Post
+    const post = data.image ? new PostImage(data) : new Post(data);
+    post.render();
+  });
+}
 
-
-post1.render();
-post2.render();
+init();
